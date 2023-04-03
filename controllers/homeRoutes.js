28194df9ffc17772e073151ef2e744bd014c930c
Jonah <jonahtkane@gmail.com>
2023-04-03 14:18:38 -0500

    added route for get / in home routes js.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,27 @@ const router = require('express').Router();
 const { Blog, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
+            inclue: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        res.render('homepage', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 // need get routes for '/' '/blog/:id' '/profile' and '/login'
 module.exports = router;
