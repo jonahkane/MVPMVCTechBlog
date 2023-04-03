46f268951313dab1f7991a81eafbdc39b800797b
Jonah <jonahtkane@gmail.com>
2023-04-03 13:56:24 -0500

    updated db name. added post route in api blog routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,6 +2,19 @@ const router = require('express').Router();
 const { Blog } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+router.post('/', withAuth, async (req, res) => {
+    try {
+        const newBlog = await Blog.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        res.status(200).json(newBlog);
+    } catch (err) {
+        console.err(err);
+        res.status(400).json(err);
+    }
+});
+
 
 
 // need to add routes for post and delete
