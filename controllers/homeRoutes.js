5165c495a2c4bb096ea67188ce176a53998f0cc0
Jonah <jonahtkane@gmail.com>
2023-04-03 14:36:32 -0500

    added route for get /profile in home routes js.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -45,5 +45,23 @@ res.render('blog', {
 }
 });
 
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password']},
+            include: [{ model: Blog }],
+        });
+        
+        const user = userData.get({ plain: true });
+
+        res.render('profile', {
+            ...user,
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 // need get routes for '/' '/blog/:id' '/profile' and '/login'
 module.exports = router;
