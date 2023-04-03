51c9567f392a91ed4b1cc0cc4f1c0e0c51d19020
Jonah <jonahtkane@gmail.com>
2023-04-03 12:56:05 -0500

    updated user js model

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 const User = require('/User');
 const Blog = require('./Blog');
 
-User.hasMany(Blog {
+User.hasMany(Blog, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-Blog.belongsTo(User {
+Blog.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
