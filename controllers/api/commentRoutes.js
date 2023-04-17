2315b82dd24aebd0845d1a32442b85d7d0273a60
Jonah <jonahtkane@gmail.com>
2023-04-16 23:46:06 -0500

    updated comment routes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -8,7 +8,7 @@ const withAuth = require('../../utils/auth');
 //Get all comments
 router.get("/", (req, res) => {
     Comment.findAll()
-        .then((dbCommentData) => res.json(dbCommentData))
+        .then((commentData) => res.json(commentData))
         .catch((err) => {
             console.log(err);
             res.status(500).json(err);
@@ -20,10 +20,10 @@ router.post('/', withAuth, (req, res) => {
     if (req.session) {
         Comment.create({
                 comment_text: req.body.comment_text,
-                post_id: req.body.post_id,
+                blog_id: req.body.blog_id,
                 user_id: req.session.user_id
             })
-            .then(dbCommentData => res.json(dbCommentData))
+            .then(commentData => res.json(commentData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
