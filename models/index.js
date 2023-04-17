b578f430297065aeaade01acb9715e25687c7948
Jonah <jonahtkane@gmail.com>
2023-04-16 23:28:37 -0500

    added comment info to index js model and complete rest

--- models/index.js
+++ models/index.js
@@ -1,13 +1,34 @@
 const User = require('./User');
 const Blog = require('./Blog');
+const Comment = require('./Comment');
 
 User.hasMany(Blog, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
-});
+})
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
 
 Blog.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };    
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Blog, {
+    foreignKey: ' blog_id'
+})
+
+
+module.exports = {
+    User,
+    Blog,
+    Comment
+};
