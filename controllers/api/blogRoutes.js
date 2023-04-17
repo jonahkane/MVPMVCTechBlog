0dcdf9b9a290b071c558cb8cea0dde27620c5d58
Jonah <jonahtkane@gmail.com>
2023-04-16 23:43:38 -0500

    updated blog routes

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,38 +1,126 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newBlog = await Blog.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newBlog);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+router.get("/", (req, res) => {
+  Blog.findAll({
+          attributes: ["id", "content", "title", "created_at"],
+          order: [
+              ["created_at", "DESC"]
+          ],
+          include: [{
+                  model: User,
+                  attributes: ["username"],
+              },
+              {
+                  model: Comment,
+                  attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+                  include: {
+                      model: User,
+                      attributes: ["username"],
+                  },
+              },
+          ],
+      })
+      .then((blogData) => res.json(blogData))
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const blogData = await Blog.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
+router.get("/:id", (req, res) => {
+  Blog.findOne({
+          where: {
+              id: req.params.id,
+          },
+          attributes: ["id", "content", "title", "created_at"],
+          include: [{
+                  model: User,
+                  attributes: ["username"],
+              },
+              {
+                  model: Comment,
+                  attributes: ["id", "comment_text", "blog_id", "user_id", "created_at"],
+                  include: {
+                      model: User,
+                      attributes: ["username"],
+                  },
+              },
+          ],
+      })
+      .then((blogData) => {
+          if (!blogData) {
+              res.status(404).json({
+                  message: "No blog found with this id"
+              });
+              return;
+          }
+          res.json(blogData);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
 
-    if (!blogData) {
-      res.status(404).json({ message: 'No blog found with this id!' });
-      return;
-    }
+router.post("/", withAuth, (req, res) => {
+  console.log("creating");
+  Blog.create({
+          title: req.body.title,
+          content: req.body.blog_content,
+          user_id: req.session.user_id
+      })
+      .then((blogData) => res.json(blogData))
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
 
-    res.status(200).json(blogData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+router.put("/:id", withAuth, (req, res) => {
+  Blog.update({
+          title: req.body.title,
+          content: req.body.blog_content,
+      }, {
+          where: {
+              id: req.params.id,
+          },
+      })
+      .then((blogData) => {
+          if (!blogData) {
+              res.status(404).json({
+                  message: "No blog found with this id"
+              });
+              return;
+          }
+          res.json(blogData);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+router.delete("/:id", withAuth, (req, res) => {
+  Blog.destroy({
+          where: {
+              id: req.params.id,
+          },
+      })
+      .then((blogData) => {
+          if (!blogData) {
+              res.status(404).json({
+                  message: "No blog found with this id"
+              });
+              return;
+          }
+          res.json(blogData);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+      });
 });
 
 module.exports = router;
