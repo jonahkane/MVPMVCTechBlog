748d745ad23ecdc6207e62b3dc4d6e2967fc9ca9
Jonah <jonahtkane@gmail.com>
2023-04-16 23:21:00 -0500

    added comment route js file

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-
+//do i need get routes for / amd /id here?????
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
