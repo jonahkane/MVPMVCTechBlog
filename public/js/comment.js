4bb976623d182aec1659bc74f52fe02500463b1b
Jonah <jonahtkane@gmail.com>
2023-04-23 15:27:16 -0500

    updated more styling. single post and comments.

--- public/js/comment.js
+++ public/js/comment.js
@@ -27,4 +27,4 @@ async function commentFormHandler(event) {
     }
 }
 
-document.querySelector('.comment-body').addEventListener('submit', commentFormHandler);
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
