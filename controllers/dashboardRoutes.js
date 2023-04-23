ee0eeb290ed206177b7d693261919313b7c1de99
Jonah <jonahtkane@gmail.com>
2023-04-23 17:10:12 -0500

    updated dashboard routes to be async

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -8,8 +8,9 @@ const {
 const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, (req, res) => {
-    Blog.findAll({
+router.get("/", withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({
             where: {
                 user_id: req.session.user_id
             },
@@ -32,24 +33,64 @@ router.get('/', withAuth, (req, res) => {
                     attributes: ['username']
                 }
             ]
-        })
-        .then(blogData => {
-            const blogs = blogData.map(blog => blog.get({
-                plain: true
-            }));
-            res.render('dashboard', {
-                blogs,
-                loggedIn: true
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
         });
+        const blogs = blogData.map(blog => blog.get({
+            plain: true
+        }));
+        res.render('dashboard', {
+            blogs,
+            loggedIn: true
+        });
+
+    } catch (err) {
+        console.log('err');
+        res.status(500).json(err);
+    }
 });
 
-router.get('/edit/:id', withAuth, (req, res) => {
-    Blog.findOne({
+// router.get('/', withAuth, (req, res) => {
+//     Blog.findAll({
+//             where: {
+//                 user_id: req.session.user_id
+//             },
+//             attributes: [
+//                 'id',
+//                 'title',
+//                 'content',
+//                 'created_at'
+//             ],
+//             include: [{
+//                     model: Comment,
+//                     attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+//                     include: {
+//                         model: User,
+//                         attributes: ['username']
+//                     }
+//                 },
+//                 {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             ]
+//         })
+//         .then(blogData => {
+//             const blogs = blogData.map(blog => blog.get({
+//                 plain: true
+//             }));
+//             res.render('dashboard', {
+//                 blogs,
+//                 loggedIn: true
+//             });
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
+
+router.get("/edit/:id", withAuth, async (req, res) => {
+    try {
+        const blogData = await Blog.findOne({
             where: {
                 id: req.params.id
             },
@@ -72,34 +113,92 @@ router.get('/edit/:id', withAuth, (req, res) => {
                     attributes: ['username']
                 }
             ]
-        })
-        .then(blogData => {
-            if (!blogData) {
-                res.status(404).json({
-                    message: 'No blog found with this id'
-                });
-                return;
-            }
-
-            const blog = blogData.get({
-                plain: true
-            });
-
-            res.render('edit-blog', {
-                blog,
-                loggedIn: true
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
         });
-})
+        if (!blogData) {
+            res.status(404).json({
+                message: "No blog found with this id"
+            });
+            return;
+        }
+        const blog = blogData.get({
+            plain: true
+        });
 
-router.get('/new', (req, res) => {
-    res.render('add-blog', {
-        loggedIn: true
-    })
-})
+        res.render('edit-blog', {
+            blog,
+            loggedIn: true
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+// router.get('/edit/:id', withAuth, (req, res) => {
+//     Blog.findOne({
+//             where: {
+//                 id: req.params.id
+//             },
+//             attributes: [
+//                 'id',
+//                 'title',
+//                 'content',
+//                 'created_at'
+//             ],
+//             include: [{
+//                     model: Comment,
+//                     attributes: ['id', 'comment_text', 'blog_id', 'user_id', 'created_at'],
+//                     include: {
+//                         model: User,
+//                         attributes: ['username']
+//                     }
+//                 },
+//                 {
+//                     model: User,
+//                     attributes: ['username']
+//                 }
+//             ]
+//         })
+//         .then(blogData => {
+//             if (!blogData) {
+//                 res.status(404).json({
+//                     message: 'No blog found with this id'
+//                 });
+//                 return;
+//             }
+
+//             const blog = blogData.get({
+//                 plain: true
+//             });
+
+//             res.render('edit-blog', {
+//                 blog,
+//                 loggedIn: true
+//             });
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// })
+
+router.get("/new", async (req, res) => {
+    try {
+        res.render('add-blog', {
+            loggedIn: true
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+// router.get('/new', (req, res) => {
+//     res.render('add-blog', {
+//         loggedIn: true
+//     })
+// })
 
 module.exports = router;
