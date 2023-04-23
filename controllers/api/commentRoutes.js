4f9537e1e153af4bf5b4455e2d2e724f486bdf85
Jonah <jonahtkane@gmail.com>
2023-04-23 16:13:00 -0500

    rewrote all functions in comment routes to be async - hoping to fix issue

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,31 +3,59 @@ const { User, Blog, Comment} = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
-//Get all comments
-router.get("/", (req, res) => {
-    Comment.findAll()
-        .then((commentData) => res.json(commentData))
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-//Create a comment
-router.post('/', withAuth, (req, res) => {
-    if (req.session) {
-        Comment.create({
-                comment_text: req.body.comment_text,
-                blog_id: req.body.blog_id,
-                user_id: req.session.user_id
-            })
-            .then(commentData => res.json(commentData))
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            });
+router.get("/", async (req, res) => {
+    try {
+        const commentData = await Comment.findAll();
+        res.json(commentData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);;
     }
 });
+
+//Get all comments
+// router.get("/", (req, res) => {
+//     Comment.findAll()
+//         .then((commentData) => res.json(commentData))
+//         .catch((err) => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
+
+//Create a comment
+
+
+router.post("/", withAuth, async (req, res) => {
+    if (req.session) {
+        try {
+        const commentData = await Comment.create({
+            comment_text: req.body.comment_text,
+                blog_id: req.body.blog_id,
+                user_id: req.session.user_id
+        });
+        res.json(commentData);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+}
+});
+
+// router.post('/', withAuth, (req, res) => {
+//     if (req.session) {
+//         Comment.create({
+//                 comment_text: req.body.comment_text,
+//                 blog_id: req.body.blog_id,
+//                 user_id: req.session.user_id
+//             })
+//             .then(commentData => res.json(commentData))
+//             .catch(err => {
+//                 console.log(err);
+//                 res.status(400).json(err);
+//             });
+//     }
+// });
 
 
 module.exports = router;
